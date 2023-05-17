14da23ae909b0160083ea8351a5b71307830c023
Mago Minimalista <contato@magominimalista.com.br>
2023-05-17 05:34:51 -0300

    Cabeçalho finalizado

--- src/components/navbar/UserMenu.tsx
+++ src/components/navbar/UserMenu.tsx
@@ -2,8 +2,18 @@
 
 import { AiOutlineMenu } from 'react-icons/ai'
 import { Avatar } from '../Avatar'
+import { useCallback, useState } from 'react'
+import { MenuItem } from './MenuItem'
 
 export const UserMenu = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    }, []);
+    
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -26,7 +36,7 @@ export const UserMenu = () => {
                 </div>
 
                 <div 
-                    onClick={() => {}}
+                    onClick={toggleOpen}
                     className="
                         p-4
                         md:py-2
@@ -49,6 +59,36 @@ export const UserMenu = () => {
                     </div>
                 </div>
             </div>
+
+            {isOpen && (
+                <div
+                    className="
+                        absolute
+                        rounded-xl
+                        shadow-md
+                        w-[40vw]
+                        md:w-3/4
+                        bg-white
+                        overflow-hidden
+                        right-0
+                        top-12
+                        text-sm
+                    "
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                            <MenuItem
+                                onClick={()=>{}}
+                                label="Sing Up"
+                            />
+                            <MenuItem
+                                onClick={()=>{}}
+                                label="Loguin"
+                            />
+                        </>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
